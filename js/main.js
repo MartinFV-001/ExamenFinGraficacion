c5d16b4edbb4143d7b2a142592383f703aa3fb0a
Martin Feria Vazquez <136926278+MartinFV-001@users.noreply.github.com>
2024-06-18 15:27:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 
-import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
+import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/FBXLoader.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
 class BasicCharacterControllerProxy {
